ac1bb35cea96909bd93e9c67e6b96895e004d15e
Ujjwal Gautam <ujjwal97@hotmail.com>
2025-09-14 12:28:22 +0530

    use mongo config

--- src/main/java/com/personalisedlearn/poseidon/config/MongoConfig.java
+++ src/main/java/com/personalisedlearn/poseidon/config/MongoConfig.java
@@ -1,11 +1,19 @@
 package com.personalisedlearn.poseidon.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import jakarta.annotation.PostConstruct;
+import org.bson.UuidRepresentation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
 @Configuration
 public class MongoConfig {
@@ -19,5 +27,27 @@ public class MongoConfig {
         // Mask the password in the URI for security
         String maskedUri = mongoUri.replaceAll("://(.*?):(.*?)@", "://$1:*****@");
         logger.info("MongoDB URI: {}", maskedUri);
+    }
+
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .build();
+        
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(MongoClient mongoClient) {
+        return new SimpleMongoClientDatabaseFactory(mongoClient, 
+            new ConnectionString(mongoUri).getDatabase());
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTemplate(mongoDatabaseFactory);
     }
 }
